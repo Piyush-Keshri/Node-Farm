98b84cfe8c3b5a2c44cdb652df1d09935e205138
Piyush Keshri <piyushkeshri383@gmail.com>
2022-03-05 20:22:01 +0530

    commit

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const fs = require('fs'); 
 
+// Blocking Synchronous Way 
+
 const textIn = fs.readFileSync('./txt/input.txt','utf-8');
 console.log(textIn);
-
 const textout = `This is what we know about the avacado : ${textIn}.\n Created on ${Date.now}`;
 fs.writeFileSync('./txt/output.txt',textout);
 console.log('File written');
+
+// Non-Blocking Synchronous Way
+fs.readFile('./txt/start.txt','utf-8',(err,data1) => {
+    fs.readFile(`./txt/${data1}.txt`,'utf-8',(err,data2) => {
+        console.log(data2);
+        fs.readFile('./txt/final.txt', 'utf-8',(err,data3) => {
+            console.log(data3);
+
+            fs.writeFile('./txt/final.txt',`${data2}\n${data3}`,'utf-8',err => {
+                console.log('You file has been written 😁');
+            });
+        });
+    });
+});
+console.log('Will read file');
